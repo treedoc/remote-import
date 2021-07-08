68e8099732bb37f2cc2079419d09e37723b219b1
Jianwu Chen <jianwchen@linkedin.com>
2021-07-08 01:05:48 -0700

    fix history file path; fix issue with 'uncaught syntaxerror: identifier 'exports' has already been declared'

--- src/remote-node.ts
+++ src/remote-node.ts
@@ -2,8 +2,11 @@
 
 import * as repl from "repl";
 import * as vm from "vm"
+import * as path from 'path'
+import { homedir } from 'os';
 
 const r = repl.start("rn> ");
+path.join(homedir(),'.node_repl_history');
 r.setupHistory(".remote_node_repl_history", () => null)
 // r.context.require = require("esm")(module/*, options*/);  // Seems this is equivalent as runInContext
 vm.runInContext("require=require('esm')(module); require('remote-import')", r.context);
